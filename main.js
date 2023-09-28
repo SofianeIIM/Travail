898acf4a0c51421f7dc9f1abb8b683ec6d8d1856
SofianeIIM <146094134+SofianeIIM@users.noreply.github.com>
2023-09-28 23:14:25 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,45 @@
-alert("ATTENTION !")
-console.log("bienvenue dans la console")
+/*
+class hero {
+    constructor(name, pv, atk, def){
+    this.name = name
+    this.pv = pv
+    this.atk = atk
+    this.def = def
+}
+attackMonster(monstre){
+    monster.hp -=this.atk
+}
+}
+
+let samy = new hero("Samy", 100, 5 , 7)
+let sofiane = new hero ("Sofiane" , 120 , 2 , 12)
+*/
+
+
+class Pokemon {
+    constructor(name, atk, def, hp){
+    this.name = name
+    this.atk = atk
+    this.def = def
+    this.pv = pv
+}
+    attackPokemon(Pokemon){
+    Pokemon.hp = this.pv - this.atk
+}}
+
+let tortipousse = new Pokemon("Tortipousse", 50 , 35 , 100)
+let tortank = new Pokemon("TorTank", 60 , 25 , 100)
+
+while(tortipousse.hp > 0 || tortank.hp > 0 ){
+    tortipousse.attackPokemon(tortank)
+    if(tortipousse.hp < 0){
+    console.log ("tortipousse est mort")
+    break
+    }
+
+    tortank.attackPokemon(tortipousse)
+    if (tortank <= 0){
+    console.log("tortank est mort")
+    break
+    }
+}
